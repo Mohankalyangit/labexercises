fb18e5868bd9723c24fdeefc2dbe4535f23a38b6
Mohankalyangit <93095659+Mohankalyangit@users.noreply.github.com>
2021-10-29 05:22:33 +0530

    Add files via upload

--- ExerciseTwo.java
+++ ExerciseTwo.java
@@ -1,42 +1,20 @@
-package sixthlab;
-
-import java.util.HashMap;
-import java.util.Map;
+package labeight;
+ import java.util.*;
 
 public class ExerciseTwo {
-	static void characterCount(String inputString) {
-		// Creating a HashMap containing char
-		// as a key and occurrences as a value
-		HashMap<Character, Integer> charCountMap = new HashMap<Character, Integer>();
-
-		// Converting given string to char array
-
-		char[] strArray = inputString.toCharArray();
-
-		// checking each char of strArray
-		for (char c : strArray) {
-			if (charCountMap.containsKey(c)) {
-
-				// If char is present in charCountMap,
-				// incrementing it's count by 1
-				charCountMap.put(c, charCountMap.get(c) + 1);
-			} else {
-
-				// If char is not present in charCountMap,
-				// putting this char to charCountMap with 1 as it's value
-				charCountMap.put(c, 1);
-			}
-		}
-
-		// Printing the charCountMap
-		for (Map.Entry entry : charCountMap.entrySet()) {
-			System.out.println(entry.getKey() + " " + entry.getValue());
-		}
-	}
-
-	// Driver Code
 	public static void main(String[] args) {
-		String str = "Ajit";
-		characterCount(str);
-	}
+	 Task t1 = new Task("Task 1");
+     Timer t = new Timer();
+     t.schedule(t1, 10000); //  executes for every 10 seconds
+  }
 }
+class Task extends TimerTask {
+  private String name;
+  public Task(String name) {
+      this.name = name;
+  }
+  public void run() {
+     System.out.println("[" + new Date() + "] " + name + ": task executed!");
+  }
+}
+
